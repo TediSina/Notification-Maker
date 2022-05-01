cbc17699301fbbd6effd19de1e7085ae0f61f305
Matete Plays <holtasina1970@gmail.com>
2022-05-01 19:43:56 +0200

    Inital commit.

---
+++ Notification Maker.py
@@ -0,0 +1,26 @@
+from win10toast import ToastNotifier
+import time
+
+toast = ToastNotifier()
+
+while True:
+    print("Notification Maker")
+
+    notification_name = input("Enter the notification name: ")
+    notification_body = input("Enter the notification body (content): ")
+    notification_duration = input("Enter the notification duration: ")
+    notification_timer = input("Choose after how many seconds to show the notification: ")
+
+    try:
+        time.sleep(int(notification_timer))
+
+        toast.show_toast(
+            notification_name,
+            notification_body,
+            duration = int(notification_duration),
+            #icon_path = "icon.ico",
+            threaded = True,
+        )
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Please try again.")
